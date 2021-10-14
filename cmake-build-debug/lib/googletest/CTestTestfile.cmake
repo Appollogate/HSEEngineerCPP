cf63176a3791b7bedd6761f037e117840f3983b1
Appollogate <dskrypina@gmail.com>
2021-10-15 01:32:20 +0300

    Deleted cmake-build-debug

--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/appollogate/Documents/CLion_Projects/BigIntegerTask/HSEEngineerCPP/lib/googletest
-# Build directory: /home/appollogate/Documents/CLion_Projects/BigIntegerTask/HSEEngineerCPP/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/appollogate/Documents/CLion_Projects/BigIntegerTask/HSEEngineerCPP/lib/googletest/googlemock
-# Build directory: /home/appollogate/Documents/CLion_Projects/BigIntegerTask/HSEEngineerCPP/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
